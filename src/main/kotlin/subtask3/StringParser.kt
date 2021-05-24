9a24ca2a17e94d7849bbecbb951d345d8c0e9d03
avm82 <1982alexvm@gmail.com>
2021-05-24 18:31:57 +0300

    feat: StringParser need fix

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -1,9 +1,19 @@
 package subtask3
 
+import java.util.*
+
 class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        var str = arrayListOf<String>()
+        inputString.asSequence().forEachIndexed { i, char ->
+            when (char) {
+                '<' -> str.add(inputString.substring(i + 1, inputString.indexOfFirst { c -> c == '>' }))
+                '[' -> str.add(inputString.substring(i + 1, inputString.indexOfFirst { c -> c == ']' }))
+                '(' -> str.add(inputString.substring(i + 1, inputString.indexOfFirst { c -> c == ')' }))
+            }
+        }
+        return arrayOf(str.)
     }
 }
